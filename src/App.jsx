915b8975fe14d2a9a4e1953225a5f6f2e03ac807
IBN <IBN@NB-082.ad.boehm-group.com>
2026-02-03 14:32:27 +0100

    Remove unused React imports from most files

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React, { Suspense, lazy } from 'react';
+import { Suspense, lazy } from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Navigation from './components/Navigation';
 import Footer from './components/Footer';
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+
 
 const BRAND_NAME = "Vœrynth Système";
 
