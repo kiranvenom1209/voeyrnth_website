b37283b9e33cd7ee434c26282ea25c311b79c499
Kiran Achari <comradekiran@macbookpro.speedport.ip>
2026-03-02 20:43:41 +0100

    style: optimize mobile hero mockups positioning and spacing

--- src/pages/de/OS.jsx
+++ src/pages/de/OS.jsx
@@ -144,7 +144,7 @@ export default function DeOS() {
                                     Es wurde für Eigentümer und Personal entwickelt: Klarer Status, schnelle Kontrolle und unaufgeregter Betrieb, selbst wenn das Internet irrelevant ist.
                                 </p>
                             </div>
-                            <div className="mt-10 flex justify-center lg:justify-start">
+                            <div className="mt-10 hidden lg:flex justify-center lg:justify-start">
                                 <a
                                     href="https://github.com/kiranvenom1209/voerynth-os"
                                     target="_blank"
@@ -160,7 +160,7 @@ export default function DeOS() {
                         </m.div>
                     </div>
 
-                    <div className="w-full lg:w-1/2 relative h-[350px] sm:h-[450px] lg:h-[600px] flex justify-center lg:justify-end items-end mt-12 lg:mt-0">
+                    <div className="w-full lg:w-1/2 relative h-[350px] sm:h-[450px] lg:h-[600px] flex justify-center lg:justify-end items-end -mt-12 lg:mt-0">
                         <m.div
                             initial={{ opacity: 0, scale: 0.9, x: 50 }}
                             animate={{ opacity: 1, scale: 1, x: 0 }}
@@ -180,6 +180,27 @@ export default function DeOS() {
                             <img src="/assets/os/mobile/Screenshot_20260302_111931_Voerynth OS.jpg" alt="Mobile Interface" className="w-full h-auto relative z-10" />
                         </m.div>
                     </div>
+
+                    {/* Mobile Only GitHub Link */}
+                    <m.div
+                        initial={{ opacity: 0, y: 20 }}
+                        animate={{ opacity: 1, y: 0 }}
+                        transition={{ duration: 0.8, delay: 0.6 }}
+                        className="w-full flex lg:hidden justify-center mt-4"
+                    >
+                        <a
+                            href="https://github.com/kiranvenom1209/voerynth-os"
+                            target="_blank"
+                            rel="noopener noreferrer"
+                            className="inline-flex items-center gap-3 px-6 py-3 bg-white/5 border border-white/15 text-white/80 hover:bg-white/10 hover:border-white/30 hover:text-white transition-all duration-200 rounded text-sm tracking-wide group"
+                        >
+                            <svg viewBox="0 0 24 24" className="w-5 h-5 fill-current opacity-70 group-hover:opacity-100 transition-opacity" xmlns="http://www.w3.org/2000/svg">
+                                <path d="M12 0C5.374 0 0 5.373 0 12c0 5.302 3.438 9.8 8.207 11.387.599.111.793-.261.793-.577v-2.234c-3.338.726-4.033-1.416-4.033-1.416-.546-1.387-1.333-1.756-1.333-1.756-1.089-.745.083-.729.083-.729 1.205.084 1.839 1.237 1.839 1.237 1.07 1.834 2.807 1.304 3.492.997.107-.775.418-1.305.762-1.604-2.665-.305-5.467-1.334-5.467-5.931 0-1.311.469-2.381 1.236-3.221-.124-.303-.535-1.524.117-3.176 0 0 1.008-.322 3.301 1.23A11.509 11.509 0 0112 5.803c1.02.005 2.047.138 3.006.404 2.291-1.552 3.297-1.23 3.297-1.23.653 1.653.242 2.874.118 3.176.77.84 1.235 1.911 1.235 3.221 0 4.609-2.807 5.624-5.479 5.921.43.372.823 1.102.823 2.222v3.293c0 .319.192.694.801.576C20.566 21.797 24 17.3 24 12c0-6.627-5.373-12-12-12z" />
+                            </svg>
+                            Auf GitHub ansehen
+                        </a>
+                    </m.div>
+
                 </div>
             </Section>
 
